36b83d70ca1e6d1a46856e18ea5a20c9e907758f
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2023-01-13 18:14:04 +0200

    Create class Matrix and library - MatrixLibrary

---
+++ Matrix/Program.cs
@@ -0,0 +1,13 @@
+﻿using MatrixLibrary;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+int n, m;
+Console.Write("Кількість рядків = ");
+n = int.Parse(Console.ReadLine());
+Console.Write("Кількість стовпців = ");
+m = int.Parse(Console.ReadLine());
+int[,] matrix = Matrix.GenerateMatrix(n, m);
+Matrix.DisplayMatrix(matrix);
---
+++ MatrixLibrary/Matrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+namespace MatrixLibrary
+{
+    public class Matrix
+    {
+        public static int[,] GenerateMatrix (int rowCount, int colCount)
+        {
+            int[,] matrix = new int[rowCount, colCount];
+            Random random = new Random();
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < colCount; j++)
+                {
+                    matrix[i,j] = random.Next(-9,9);
+                }
+            return matrix;
+        }
+        public static void DisplayMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{ matrix[i, j],3}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
